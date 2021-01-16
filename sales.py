27cbb2860231e1ec84c89a390470cb2a0aa344f6
gunay <63545332+gunay-ux@users.noreply.github.com>
2021-01-17 00:54:53 +0300

    Update sales.py

--- sales.py
+++ sales.py
@@ -9,11 +9,12 @@ mnt=["January","February","March","April","May","June","July","August","Septembe
 
 layout=[[sg.Text("Analysis")],
     [sg.Text('buying amount'),
-     sg.InputText(key="_number1_",size=(20,10)),
-     sg.Text('sales amount'), sg.InputText(key="_number2_",size=(20,10)),
+     sg.InputText(key="_number1_",size=(10,10)),
+     sg.Text('sales amount'), sg.InputText(key="_number2_",size=(10,10)),
      sg.Drop(values=(mnt),key="_listbox_", auto_size_text=True,default_value=mnt[0]),
      sg.Button("Save information",key="_button_"),
-     sg.Button("comparison graphic",key="_graphic_")]]
+     sg.Button("comparison graphic",enable_events=True,key="_graphic_"),
+     sg.Button("Safe Exit",key="_Exit_")]]
 
 
 
@@ -22,33 +23,41 @@ stslist = []
 alslist = []
 months = [""]
 gainlist = ["0"]
+
+
+
 while True:
  event, values = window.read()
 
- if event=="_button_":
-      if values["_number1_"].isalpha() or values["_number2_"].isalpha():
-          sg.Popup("Wrong entry.Please enter number")
+ if event =="_button_":
+
+
+      if values["_number1_"] == "" or values["_number2_"] == "":
+          sg.Popup("! Login information is missing. Try again")
       else:
-          data1 = ""
-          for i in (values["_number1_"]):
-              data1 += i
-          stslist.append(data1)
-          data2 = ""
-          for a in (values["_number2_"]):
-              data2 += a
-          alslist.append(data2)
-          month = ""
-          for m in (values["_listbox_"]):
-              month += m
-          months.append(month)
+          if values["_number1_"].isalpha() or values["_number2_"].isalpha():
+              sg.Popup("! Wrong entry.Please enter number")
+          else:
+              data1 = ""
+              for i in (values["_number1_"]):
+                  data1 += i
+              stslist.append(data1)
+              data2 = ""
+              for a in (values["_number2_"]):
+                  data2 += a
+              alslist.append(data2)
+              month = ""
+              for m in (values["_listbox_"]):
+                  month += m
+              months.append(month)
 
-          gainValues = ""
+              gainValues = ""
 
-          gain = round((float(values["_number2_"]) - float(values["_number1_"])) * 100) / (float(values["_number1_"]))
-          for k in str(gain):
-              gainValues += k
-          gainlist.append(gainValues)
-
+              gain = round((float(values["_number2_"]) - float(values["_number1_"])) * 100) / (
+                  float(values["_number1_"]))
+              for k in str(gain):
+                  gainValues += k
+              gainlist.append(gainValues)
 
 
  def graphic():
@@ -59,8 +68,18 @@ while True:
    plt.show()
 
 
- if event== "_graphic_":
-   graphic()
 
+
+ if event== "_graphic_":
+     if values["_number1_"] == "" or values["_number2_"] == "":
+         sg.Popup("!please save the information first ")
+     else:
+         graphic()
+         break
  if event == sg.WIN_CLOSED:
+     window.close()
+
+
+ if event=="_Exit_":
+    window.close()
     break
